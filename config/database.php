7761eee5ccc5d34efd0f026c31b6b70248c479d0
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2024-10-17 12:13:14 +0200

    add `REDIS_HOST` & `REDIS_PORT` to `config/database.php`

--- config/database.php
+++ config/database.php
@@ -5,3 +5,6 @@ define('PORT', '3306');
 define('USER', 'root');
 define('PASSWORD', 'jonas2904');
 define('DATABASE', 'sasin91');
+
+define('REDIS_HOST', '127.0.0.1');
+define('REDIS_PORT', '6379');
--- modules/websocket/server.php
+++ modules/websocket/server.php
@@ -1,7 +1,11 @@
 <?php
 
+require_once __DIR__ . '/../../config/database.php';
 require_once __DIR__ . '/runtime/WebsocketServer.php';
 
-$server = new WebsocketServer();
+$server = new WebsocketServer(
+    redis_host: REDIS_HOST,
+    redis_port: REDIS_PORT,
+);
 
 $server->listen();
