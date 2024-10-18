f21c1495f0c983f3992bb6cb48e170d2ca014a02
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2024-10-18 16:15:21 +0200

    added `validation` typehint to Trongate

--- engine/Trongate.php
+++ engine/Trongate.php
@@ -4,6 +4,7 @@
  * Manages loading modules and rendering of HTML templates.
  * Also contains methods for assisting with file uploads.
  * @property-read Model $model exposes the Model class to intellisense
+ * @property-read Validation $validation
  */
 class Trongate {
 
