093076194241ea813e4f9ccbfbd1bcebc3b46aec
Jonas Kervin hansen <jonas.kerwin.hansen@gmail.com>
2025-02-24 12:02:53 +0100

    bugfix: missing config values for websocket module

--- config/websocket.php
+++ config/websocket.php
@@ -1,4 +1,8 @@
 <?php
 
+define('REDIS_HOST', $_ENV['REDIS_HOST'] ?? '127.0.0.1');
+define('REDIS_PORT', $_ENV['REDIS_PORT'] ?? '6379');
+
 define('WEBSOCKET_HOST', $_ENV['WEBSOCKET_HOST'] ?? '127.0.0.1');
 define('WEBSOCKET_PORT', $_ENV['WEBSOCKET_PORT'] ?? 8085);
+
