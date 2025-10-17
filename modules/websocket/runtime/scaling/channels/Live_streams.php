1d1b86fb526946ba6b4763fd900152b6989dd474
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2025-10-17 15:23:54 +0200

    feat(websocket): live_streams channel

--- modules/websocket/runtime/scaling/channels/Live_streams.php
+++ modules/websocket/runtime/scaling/channels/Live_streams.php
@@ -2,5 +2,24 @@
 
 class Live_streams extends Channel
 {
-    //
+    public function listen()
+    {
+        $subscriber = $this->messenger->get_subscriber();
+
+        // Ensure connection is established
+        if (!$subscriber->connection) {
+            $subscriber->connect();
+        }
+
+        $fiber = new Fiber($this->json_subscription(
+            $this->name(),
+            function($channel, $message) {
+                $this->messenger->broadcast($channel, $message);
+            }
+        ));
+
+        $fiber->start($subscriber->connection);
+
+        $this->fibers->enqueue($fiber);
+    }
 }
